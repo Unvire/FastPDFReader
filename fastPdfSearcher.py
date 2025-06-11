87c3362d965a909ad244cd0de36fbe7e801924c9
Unvire <krzysztof.balcerzak6@gmail.com>
2025-06-11 14:06:03 +0200

    Adds function for spliting list to n chunks

--- fastPdfSearcher.py
+++ fastPdfSearcher.py
@@ -1,7 +1,9 @@
 import os
-from multiprocessing import Pool
 import fitz
 import time
+
+import multiprocessing
+from multiprocessing import Pool
 
 class FastPdfSearcher:
     def __init__(self):
@@ -11,8 +13,17 @@ class FastPdfSearcher:
     def _elapsedTime(self) -> float:
         return time.time() - self.timeStamp
     
-    def _calculateNumberOfProcesses()
-
+    def _calculateNumberOfProcesses(self) -> int:
+        return multiprocessing.cpu_count()
+    
+    def _splitFilesListToChunks(self, pdfFileNames:list[str], numOfChunks:int) -> list[list[str]]:
+        result = [[] for _ in range(numOfChunks)]
+        fileNamesCopy = pdfFileNames[:]
+        while fileNamesCopy:
+            chunk = result.pop(0)
+            chunk.append(fileNamesCopy.pop(0))
+            result.append(chunk)
+        return result
 
 if __name__ == '__main__':
-    pass
+    instance = FastPdfSearcher()
